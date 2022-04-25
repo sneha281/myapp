cb07892dd7e683ab6396839560579a4335c7a0f1
bhomik yadav <bhomickyadav786@gmail.com>
2022-04-25 19:59:34 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,33 +1,26 @@
-
-import './App.css';
+import "./App.css";
 import React from "react";
 
-
-import NavBar from './components/NavBar'
-import About from './components/About';
-import Home from './components/Home';
-import { Router , Route } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from "./components/NavBar";
+import About from "./components/About";
+import Home from "./components/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-        
-       
-       <Router>
-        <NavBar/>
-       
-         <Route path='/welcome' element={<Home/>} />
-            
-         
-         <Route path='/welcome' element={<About/>} />
-         
-        
-       </Router>
-    
-    </div>
-      
-  
+    <>
+      <BrowserRouter>
+        <NavBar />
+        <div className="App">
+          <Routes>
+            <Route exact path="/home" element={<Home />} />
+
+            <Route exact path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,32 +1,37 @@
-import React from 'react';
+import React from "react";
 import * as ReactBootStrap from "react-bootstrap";
-import {
-    Link
-  } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-    return(
-        <div className="App">
-    <ReactBootStrap.Navbar collapseOnSelect expand="xl" bg="danger" variant="dark">
-  <ReactBootStrap.Navbar.Brand href="#home">THICC BOIS HOURS</ReactBootStrap.Navbar.Brand>
-  <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
-    <ReactBootStrap.Nav className="mr-auto"> 
-    <Link to="/Home">
-    <ReactBootStrap.Nav.Link href="#Home">Home</ReactBootStrap.Nav.Link>
-    </Link>
-    <Link to="/About">
-    <ReactBootStrap.Nav.Link href="#About">About</ReactBootStrap.Nav.Link>
-    </Link>
-    
-
-    
-   
-    </ReactBootStrap.Nav>
-  </ReactBootStrap.Navbar.Collapse>
-</ReactBootStrap.Navbar>
-        </div>
-    )
-}
+  return (
+    <div className="App">
+      <ReactBootStrap.Navbar
+        collapseOnSelect
+        expand="xl"
+        bg="danger"
+        variant="dark"
+      >
+        <ReactBootStrap.Navbar.Brand href="#home">
+          THICC BOIS HOURS
+        </ReactBootStrap.Navbar.Brand>
+        <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
+          <ReactBootStrap.Nav className="mr-auto">
+            <Link to="/home">
+              <ReactBootStrap.Nav.Link href="#Home">
+                Home
+              </ReactBootStrap.Nav.Link>
+            </Link>
+            <Link to="/about">
+              <ReactBootStrap.Nav.Link href="#About">
+                About
+              </ReactBootStrap.Nav.Link>
+            </Link>
+          </ReactBootStrap.Nav>
+        </ReactBootStrap.Navbar.Collapse>
+      </ReactBootStrap.Navbar>
+    </div>
+  );
+};
 
 export default NavBar;
